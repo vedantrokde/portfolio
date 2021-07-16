96d2358d65f857f8b89bc161a9b431afcc00d173
vedantrokde <vedant.rokde2018@vitstudent.ac.in>
2021-07-17 00:33:26 +0530

    add form-mailer

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -56,7 +56,7 @@
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
     .then(response => {
-      if( response.ok ) {
+      if( response.status==200 ) {
         return response.text()
       } else {
         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
@@ -64,7 +64,10 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      data = JSON.parse(data);
+
+      if (data['ok']===true) {
+        console.log("yes");
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
